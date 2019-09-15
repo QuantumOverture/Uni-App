c7112c87b32176b8981251a984760b1de342b0b6
ismaildude <38566647+ismaildude@users.noreply.github.com>
2019-09-14 23:29:05 -0700

    All API Data IS IN (so far...)

--- views.py
+++ views.py
@@ -5,16 +5,39 @@ from .forms import RedirectToUni
 import json
 import requests
 
+
+def RemoveExtraInfo(Major_String):
+    OpenBracketIndex = -1
+    Index = 0
+    while Index <= len(Major_String)-1:
+        if Major_String[Index] == "(":
+            OpenBracketIndex = Index
+            Index += 1
+        elif Major_String[Index] ==")":
+            Major_String = Major_String[:OpenBracketIndex] + Major_String[Index+1:]
+            OpenBracketIndex = -1
+            Index = 0
+        else:
+            Index += 1
+
+
+    return Major_String
+
+
 def MajorNamesList(Majors):
-    pass
+    # Convert into proper format here for api call
+    # (ANTHING BETWEEN THESE TWO IS EXTRA INFO AND NOT PART OF API CALL)
+    # Replace _ with spaces when providing extra info
+    ResultString = ""
+    for i in Majors:
+        ResultString += "latest.academics.program_percentage."+RemoveExtraInfo(i)+","
+
+    return ResultString
 
 
 @sensitive_variables()
 def SpecificUni(request,NAME,ID):
-    # (ANTHING BETWEEN THESE TWO IS EXTRA INFO AND NOT PART OF API CALL)
-    # Replace _ with spaces when providing extra info
-    Majors = [
-              "agriculture","(natural_)resources(_and_conservation)","architecture","ethnic_cultural_gender(_studies)",
+    Majors = ["agriculture","(natural_)resources(_and_conservation)","architecture","ethnic_cultural_gender(_studies)",
               "communication","communications_technology","computer(_science_and_information studies)","personal(_and)_culinary(_studies)",
               "education","engineering","engineering_technology(_and_engineering_related field)","(foreign_)language(_,literature,and_linguistics)",
               "family(_and)_consumer_science","legal(_profession_and_studies)","english","humanities","library(_science)","biological(_and_biomedical_science)",
@@ -22,11 +45,10 @@ def SpecificUni(request,NAME,ID):
               "philosophy_religious","theology_religious_vocation","physical_science","science_technology","psychology","security_law_enforcement",
               "public_administration(_and)_social_service","social_science","construction","mechanic_(and_)repair_technology",
               "precision_production","transportation","visual_performing(_arts)","health","business_marketing","history"
-             
              ]
     # Here forward to ID - Star rating - Text Review in Database fields
     # error check here if ID of school exists or not and NAME  exists in the the university's title
-    # ApiKey = PLEASE GO TO https://api.data.gov
+    # ApiKey = 'PLEASE GO TO API.DATA.GOV for a key or search for it on Google or your favorite search engine'
     URL = 'https://api.data.gov/ed/collegescorecard/v1/schools?'
     Fields = "&_fields=school.name,school.state," \
              "school.ownership,latest.admissions.admission_rate.overall," \
@@ -37,15 +59,15 @@ def SpecificUni(request,NAME,ID):
              "latest.student.grad_students,latest.cost.program_reporter.program_1.cip_6_digit.full_program," \
              "school.region_id,school.degree_urbanization,school.school_url," \
              "school.carnegie_basic,school.carnegie_size_setting,school.city," \
-             "student.demographics.first_generation,completion.completion_rate_4yr_100nt," \
-             "school.religious_affiliation,student.demographics.race_ethnicity.white," \
-             "student.demographics.race_ethnicity.black,student.demographics.race_ethnicity.asian" \
-             "student.demographics.race_ethnicity.hispanic,student.demographics.race_ethnicity.aian," \
-             "student.demographics.race_ethnicity.nhpi"+ MajorNamesList(Majors)
+             "latest.student.demographics.first_generation,latest.completion.completion_rate_4yr_100nt," \
+             "school.religious_affiliation,latest.student.demographics.race_ethnicity.white," \
+             "latest.student.demographics.race_ethnicity.black,latest.student.demographics.race_ethnicity.asian," \
+             "latest.student.demographics.race_ethnicity.hispanic,latest.student.demographics.race_ethnicity.aian," \
+             "latest.student.demographics.race_ethnicity.nhpi,latest.student.demographics.men,latest.student.demographics.women,"+MajorNamesList(Majors)
 
     WebSiteResponse = requests.get(URL+'id='+str(ID)+Fields+"&api_key="+ApiKey)
     WebsiteJSON = json.loads(WebSiteResponse.text)
-    # Add men and women stats
+
     content = {
         "ID": ID,
         "NAME":NAME,
@@ -69,13 +91,28 @@ def SpecificUni(request,NAME,ID):
         "school_type":WebsiteJSON["results"][0]["school.carnegie_basic"],
         "setting_size":WebsiteJSON["results"][0]["school.carnegie_size_setting"],
         "city":WebsiteJSON["results"][0]["school.city"],
+        "percent_men": WebsiteJSON["results"][0]["latest.student.demographics.men"],
+        "percent_women": WebsiteJSON["results"][0]["latest.student.demographics.women"],
+        "first_gen": WebsiteJSON["results"][0]["latest.student.demographics.first_generation"],
+        "four_year_completion":WebsiteJSON["results"][0]["latest.completion.completion_rate_4yr_100nt"],
+        "religious_affiliation":WebsiteJSON["results"][0]["school.religious_affiliation"],
+        "race_white":WebsiteJSON["results"][0]["latest.student.demographics.race_ethnicity.white"],
+        "race_black": WebsiteJSON["results"][0]["latest.student.demographics.race_ethnicity.black"],
+        "race_asian": WebsiteJSON["results"][0]["latest.student.demographics.race_ethnicity.asian"],
+        "race_aian": WebsiteJSON["results"][0]["latest.student.demographics.race_ethnicity.aian"],
+        "race_nhpi": WebsiteJSON["results"][0]["latest.student.demographics.race_ethnicity.nhpi"],
+        "race_hispanic": WebsiteJSON["results"][0]["latest.student.demographics.race_ethnicity.hispanic"],
     }
-    print(WebSiteResponse.text)
+
+    # add percentage of majors to content
+    for z in Majors:
+        content[RemoveExtraInfo(z)] = WebsiteJSON["results"][0]["latest.academics.program_percentage."+RemoveExtraInfo(z)] * 100
+
     return render(request,'MainApp/SpecificUni.html',content)
 
 @sensitive_variables()
 def UniReport(request,UniName):
-    # ApiKey = PLEASE GO TO https://api.data.gov
+    # ApiKey = 'PLEASE GO TO API.DATA.GOV for a key or search for it on Google or your favorite search engine'
     URL = 'https://api.data.gov/ed/collegescorecard/v1/schools?'
     UniName = UniName.replace(" ","%20")
     Fields = "&_fields=school.name,id,school.state,school.ownership"
@@ -85,7 +122,7 @@ def UniReport(request,UniName):
 
 
     content = { "UniName":UniName,"UniList" :[] }
-    print(WebsiteJSON)
+
     for i in WebsiteJSON["results"]:
         content["UniList"].append({"schoolname":i["school.name"],"ID":i["id"],"state":i["school.state"],"ownership":i["school.ownership"]})
 
