825762f5a9297009377c6f36bdfde2654195ccd1
ismaildude <38566647+ismaildude@users.noreply.github.com>
2019-09-21 15:16:09 -0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('result/<str:UniName>',views.UniReport,name ="UniReport"),
-    path('result/<str:NAME>/<int:ID>',views.SpecificUni,name ="SpecificUni"),
+    path('result/<str:UniName>/<int:PgNum>',views.UniReport,name ="UniReport"),
+    path('result/<str:NAME>/<int:PgNum>/<int:ID>',views.SpecificUni,name ="SpecificUni"),
     path('',views.HomePage, name="HomePage")
 
 
