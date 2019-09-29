0d948aeb5f033d166abf411da54bc3f00a23d3e5
ismaildude <38566647+ismaildude@users.noreply.github.com>
2019-09-28 17:12:35 -0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,17 @@
 from django import forms
-
+from MainApp.models import UniComment
 
 
 class RedirectToUni(forms.Form):
     UniName = forms.CharField(label="Enter University/College Name: ",required=True)
+
+CHOICES = [(1,'1 Star'),
+           (2,'2 Star'),
+           (3,'3 Star'),
+           (4,'4 Star'),
+           (5,'5 Star'),]
+class UserReview(forms.ModelForm):
+    StarRating = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
+    class Meta:
+        model = UniComment
+        fields = ['Comment','StarRating']
